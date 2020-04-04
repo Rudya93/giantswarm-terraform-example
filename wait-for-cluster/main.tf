ad53eb724d9a0c7b71c1556c340f4d490abc8fbd
Simon Weald <glitchcrab-github@simonweald.com>
2020-04-04 12:19:43 +0100

    correct missing bracket

--- wait-for-cluster/main.tf
+++ wait-for-cluster/main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "cluster_status" {
   provisioner "local-exec" {
     # this uses some regex lookahead magic to work; it could potentially be brittle
     # depending on the version of grep available to you locally.
-    command = "until [ `curl -sH \"Authorization: giantswarm ${var.auth_token}\" ${var.api_uri}/v5/clusters/${var.clusterid}/nodepools/${var.nodepoolid}/ | grep -oP '(?<=\"nodes_ready\":)[^}]*'` -gt 0 ; do sleep 30 ; done"
+    command = "until [ `curl -sH \"Authorization: giantswarm ${var.auth_token}\" ${var.api_uri}/v5/clusters/${var.clusterid}/nodepools/${var.nodepoolid}/ | grep -oP '(?<=\"nodes_ready\":)[^}]*'` -gt 0 ] ; do sleep 30 ; done"
     # using jq is more robust, but may not be available to you locally. if you can
     # use jq instead of grep then this is recommended.
     # command = "until [ `curl -sH \"Authorization: giantswarm ${var.auth_token}\" ${var.api_uri}/v5/clusters/${var.clusterid}/nodepools/${var.nodepoolid}/ | jq .status.nodes_ready` -gt 0 ; do sleep 30 ; done"
