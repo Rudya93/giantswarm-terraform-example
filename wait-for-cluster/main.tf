6feaa6bb009c998a147acd0c8c69954cb7762005
Simon Weald <glitchcrab-github@simonweald.com>
2020-04-04 09:38:39 +0100

    wait for the nodepool's nodes to register as ready

--- wait-for-cluster/main.tf
+++ wait-for-cluster/main.tf
@@ -3,6 +3,6 @@ resource "null_resource" "cluster_status" {
   depends_on       = [var.nodepoolid]
 
   provisioner "local-exec" {
-    command = "until curl -sH \"Authorization: giantswarm ${var.auth_token}\" ${var.api_uri}/v5/clusters/${var.clusterid}/ | grep -m 1 \"Created\" ; do sleep 30 ; done"
+    command = "until [ `curl -sH \"Authorization: giantswarm ${var.auth_token}\" ${var.api_uri}/v5/clusters/${var.clusterid}/nodepools/${var.nodepoolid}/ | jq .status.nodes_ready` -gt 0 ; do sleep 30 ; done"
   }
 }
